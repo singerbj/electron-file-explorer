7143c25e66abdda9edebe9898db0b3f55af5269b
Ben Singer <benjamin.singer@helpsystems.com>
2016-10-17 09:30:39 -0500

    changes

--- renderer.js
+++ renderer.js
@@ -4,6 +4,8 @@
 // All of the Node.js APIs are available in this process.
 
 var fs = require('fs');
+var util = require('util');
+var exec = require('child_process').exec;
 
 // var seperator = '/';
 // if (process.platform === 'win32') {
@@ -34,13 +36,17 @@ var getFilesForDir = function (dir, goingUpDir) {
 
     console.log(upDir, dir);
 
+    $('.currentDir').text(dir);
+
     currentDir = dir;
     $('.items').empty();
     fs.readdir(dir, function (err, items) {
         if (err) {
             console.error(err);
         } else {
-            $('.items').append('<li><a href class="folder-link" data-file="' + upDir + '" data-updir="true">..</a></li>');
+            $('.items').append('<div><i class="fa fa-level-up" aria-hidden="true"></i>&nbsp;&nbsp;<a href class="folder-link" data-file="' + upDir + '" data-updir="true">..</a></div>');
+            var files = [];
+            var folders = [];
             items.forEach(function (item) {
                 var path;
                 if (currentDir[currentDir.length - 1] !== '/') {
@@ -49,10 +55,17 @@ var getFilesForDir = function (dir, goingUpDir) {
                     path = currentDir + item;
                 }
                 if (item.indexOf('.') < 0) {
-                    $('.items').append('<li><a href class="folder-link" data-file="' + path + '">' + item + '</a></li>');
+                    folders.push('<div><i class="fa fa-folder" aria-hidden="true">&nbsp;&nbsp;</i><a href class="folder-link" data-file="' + path + '">' + item + '</a></div>');
                 } else {
-                    $('.items').append('<li>' + item + '</li>');
+                    files.push('<div><i class="fa fa-file" aria-hidden="true">&nbsp;&nbsp;</i><a href class="file-link" data-file="' + path + '">' + item + '</a></div>');
                 }
+            });
+
+            folders.forEach(function (folder) {
+                $('.items').append(folder);
+            });
+            files.forEach(function (file) {
+                $('.items').append(file);
             });
         }
     });
@@ -65,5 +78,19 @@ $('body').click(function (e) {
     var tgt = $(e.target);
     if (tgt.hasClass('folder-link')) {
         getFilesForDir(tgt.data('file'), tgt.data('updir'));
+    } else if (tgt.hasClass('file-link')) {
+        // console.log(getCommandLine() + ' ' + tgt.data('file'));
+        // exec(getCommandLine() + ' ' + tgt.data('file'));
+        var shell = require('electron').shell;
+        // var path = require('path');
+
+        var r;
+        if (process.platform === "win32" || process.platform === "win64") {
+            r = shell.openItem(tgt.data('file').replace(/\//, "\\"));
+        } else {
+            r = shell.openItem(tgt.data('file'));
+        }
+
+        console.log(r, tgt.data('file'));
     }
 });
